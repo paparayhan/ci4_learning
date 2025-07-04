0d0447e17e1cbb6edc631e8dd7c9a5fafbdde639
paparayhan <mie.developer85@gmail.com>
2025-07-04 14:48:37 +0700

    update database migration

--- app/Database/Migrations/2025-07-04-020912_Posts.php
+++ app/Database/Migrations/2025-07-04-020912_Posts.php
@@ -9,14 +9,14 @@ class Posts extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id'=>['type'=>'INT','unisgned'=>TRUE,'auto_incement'=>TRUE],
+            'id'=>['type'=>'INT','unsigned'=>TRUE,'auto_increment'=>TRUE],
             'title'=>['type'=>'VARCHAR','constraint'=>128, 'null'=>FALSE],
             'content'=>['type'=>'TEXT','null'=>FALSE],
             'slug'=>['type'=>'VARCHAR','constraint'=>128,'null'=>FALSE],
             'status'=>['type'=>'INT','constraint'=>1,'null'=>FALSE],
-            'created_at'=>['type'=>'datetime','null'=>FALSE],
-            'updated_at'=>['type'=>'datetime','null'=>FALSE],
-            'deleted_at'=>['type'=>'datetime','null'=>FALSE]
+            'created_at'=>['type'=>'datetime','null'=>TRUE],
+            'updated_at'=>['type'=>'datetime','null'=>TRUE],
+            'deleted_at'=>['type'=>'datetime','null'=>TRUE]
         ]);
         $this->forge->addKey('id',TRUE);
         $this->forge->createTable('posts');
